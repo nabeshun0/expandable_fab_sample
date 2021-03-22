91d0705272c89d9e06a0d374f43a64115f2c3e24
snt-nbzw <sntnbzw@gmail.com>
2021-03-23 00:00:45 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/nabeshun/expandable_fab_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nabeshun.expandable_fab_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ExampleExpandableFab(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+@immutable
+class ExampleExpandableFab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

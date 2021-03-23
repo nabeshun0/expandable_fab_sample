51ad2cec04732210819054f19ce2dc2b52527859
snt-nbzw <sntnbzw@gmail.com>
2021-03-24 00:22:30 +0900

    Add: expand and collapse the action buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(
@@ -85,18 +86,44 @@ class ExpandableFab extends StatefulWidget {
   _ExpandableFabState createState() => _ExpandableFabState();
 }
 
-class _ExpandableFabState extends State<ExpandableFab> {
+class _ExpandableFabState extends State<ExpandableFab>
+    with SingleTickerProviderStateMixin {
   bool _open = false;
+
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
 
   @override
   void initState() {
     super.initState();
     _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   void _toggle() {
     setState(() {
       _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
     });
   }
 
@@ -108,6 +135,7 @@ class _ExpandableFabState extends State<ExpandableFab> {
         clipBehavior: Clip.none,
         children: [
           _buildTapToCloseFab(),
+          ..._buildExpandingActionButtons(),
           _buildTapToOpenFab(),
         ],
       ),
@@ -138,6 +166,25 @@ class _ExpandableFabState extends State<ExpandableFab> {
     );
   }
 
+  List<Widget> _buildExpandingActionButtons() {
+    final children = <Widget>[];
+    final count = widget.children.length;
+    final step = 90.0 / (count - 1);
+    for (var i = 0, angleInDegrees = 0.0;
+        i < count;
+        i++, angleInDegrees += step) {
+      children.add(
+        _ExpandingActionButton(
+          directionInDegrees: angleInDegrees,
+          maxDistance: widget.distance,
+          progress: _expandAnimation,
+          child: widget.children[i],
+        ),
+      );
+    }
+    return children;
+  }
+
   Widget _buildTapToOpenFab() {
     return IgnorePointer(
       ignoring: _open,
@@ -159,6 +206,46 @@ class _ExpandableFabState extends State<ExpandableFab> {
             child: const Icon(Icons.create),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _ExpandingActionButton extends StatelessWidget {
+  _ExpandingActionButton({
+    Key? key,
+    required this.directionInDegrees,
+    required this.maxDistance,
+    required this.progress,
+    required this.child,
+  }) : super(key: key);
+
+  final double directionInDegrees;
+  final double maxDistance;
+  final Animation<double> progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress,
+      builder: (BuildContext context, Widget? child) {
+        final offset = Offset.fromDirection(
+          directionInDegrees * (math.pi / 180.0),
+          progress.value * maxDistance,
+        );
+        return Positioned(
+          right: 4.0 + offset.dx,
+          bottom: 4.0 + offset.dy,
+          child: Transform.rotate(
+            angle: (1.0 - progress.value) * math.pi / 2,
+            child: child!,
+          ),
+        );
+      },
+      child: FadeTransition(
+        opacity: progress,
+        child: child,
       ),
     );
   }
